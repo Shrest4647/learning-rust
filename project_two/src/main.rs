ae9edd61d253e8ded001b4ab5e7123848c30ba13
Sunil Shrestha <sunil.shrestha@lisnepal.com.np>
2022-09-22 23:49:56 +0545

    Completed the number guessing game

--- project_two/src/main.rs
+++ project_two/src/main.rs
@@ -1,3 +1,33 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("Please input your Guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                // eprintln!("That's not a number");
+                continue;
+            },
+            };
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too Low"),
+            std::cmp::Ordering::Equal => { 
+                println!("You Win");
+                break;
+            },
+            std::cmp::Ordering::Greater => println!("Too High"),
+        }
+        println!("You guessed: {}", guess);
+    }
 }
